9d9f203af800958674b8750db1c7bee5e630e0af
Donald <jimjerry00b@gmail.com>
2025-02-28 13:23:21 +0600

    init commit

---
+++ app/Models/RoleModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RoleModel extends Model
+{
+
+    protected $table = 'roles';
+    protected $primaryKey = 'id';
+    protected $guarded =[];
+
+
+    public function permissions(){
+        // return $this->belongsToMany(PermissionModel::class, 'permission_role');
+        return $this->belongsToMany(PermissionModel::class, 'permission_role', 'role_id', 'permission_id');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\PermissionToRoleController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'home'])->name('home.page');
+
+Route::middleware('guest')->group(function(){
+    Route::get('/login', [HomeController::class, 'login'])->name('login.page');
+    Route::post('/login-post', [HomeController::class, 'loginPost'])->name('login.post');
+});
+
+
+
+Route::middleware('admin')->group(function(){
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
+    Route::resource('user',UserController::class);
+    Route::resource('role',RoleController::class);
+    Route::resource('permission',PermissionController::class);
+    Route::resource('permission_to_role',PermissionToRoleController::class);
+});
